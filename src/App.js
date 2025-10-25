a18e7cb019f36183b6b329233d1eb1f4230301a1
yshls <shluxnsal01@gmail.com>
2025-10-25 21:26:16 +0900

    feat(Project): improve input prompts and refine validation logic

--- src/App.js
+++ src/App.js
@@ -2,27 +2,27 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
-    const carNamesInput = await MissionUtils.Console.readLineAsync(
+    MissionUtils.Console.print(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
     );
+    const carNamesInput = await MissionUtils.Console.readLineAsync('');
 
     const carNamesSplit = carNamesInput.split(',').map((name) => name.trim());
 
     let nameInvalid = carNamesSplit.some(
-      (name) => name.length > 5 || name.length === 0
+      (name) => name.length > 5 || name.length < 1
     );
 
     if (nameInvalid) {
       throw new Error('[ERROR]');
     }
 
-    const numberCountInput = await MissionUtils.Console.readLineAsync(
-      '시도할 횟수는 몇 회인가요?'
-    );
+    MissionUtils.Console.print('시도할 횟수는 몇 회인가요?');
+    const numberCountInput = await MissionUtils.Console.readLineAsync('');
 
     const numberCount = Number(numberCountInput);
     const numberCountInvalid =
-      isNaN(numberCount) || numberCount <= 0 || !Number.isInteger(numberCount);
+      isNaN(numberCount) || numberCount < 1 || !Number.isInteger(numberCount);
 
     if (numberCountInvalid) {
       throw new Error('[ERROR]');
