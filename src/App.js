49ee22b5f2bdb36fac7007b5f55a1a815ff2e90d
yshls <shluxnsal01@gmail.com>
2025-10-25 21:45:25 +0900

    feat(Project): add result display logic for car positions after race

--- src/App.js
+++ src/App.js
@@ -43,6 +43,11 @@ class App {
         }
       });
     }
+
+    cars.forEach((car) => {
+      const result = `${car.name} : ${'-'.repeat(car.position)}`;
+      MissionUtils.Console.print(result);
+    });
   }
 }
 
