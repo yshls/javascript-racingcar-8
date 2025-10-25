0acd333e5aa29a1e1f6a393e7a3dfbc288fafbe7
yshls <shluxnsal01@gmail.com>
2025-10-25 19:08:28 +0900

    feat(Parser): split car names by comma

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ class App {
     const carNamesInput = await MissionUtils.Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)'
     );
+
+    const carNamesSplit = carNamesInput.split(',').map((name) => name.trim());
   }
 }
 
