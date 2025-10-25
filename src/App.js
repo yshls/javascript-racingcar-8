7610c651f49b269bc8f39b796ff56723ada0a5bb
yshls <shluxnsal01@gmail.com>
2025-10-25 21:29:50 +0900

    fix(Project): change nameInvalid variable declaration to const for better readability

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App {
 
     const carNamesSplit = carNamesInput.split(',').map((name) => name.trim());
 
-    let nameInvalid = carNamesSplit.some(
+    const nameInvalid = carNamesSplit.some(
       (name) => name.length > 5 || name.length < 1
     );
 
